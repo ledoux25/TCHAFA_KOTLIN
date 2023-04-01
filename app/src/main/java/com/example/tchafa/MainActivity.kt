44199bf1946f9adf41a14bf2d53b5d2c4686af45
azeukeng-brondon <brondongines@gmail.com>
2023-04-01 10:15:03 -0700

    Publication Screen

--- app/src/main/java/com/example/tchafa/MainActivity.kt
+++ app/src/main/java/com/example/tchafa/MainActivity.kt
@@ -3,41 +3,46 @@ package com.example.tchafa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tchafa.ui.theme.TCHAFATheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TCHAFATheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        mainView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun mainView()
+{
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+                           .fillMaxHeight()
+                           .fillMaxSize()
+                           .padding(8.dp),
+        color = Color.Gray
+    ) {
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TCHAFATheme {
-        Greeting("Android")
-    }
+    mainView()
 }
