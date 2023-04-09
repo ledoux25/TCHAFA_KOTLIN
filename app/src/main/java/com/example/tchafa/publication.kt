5a737f0edbd58d0e361fbe5da2e366c792fd2fae
azeukeng-brondon <brondongines@gmail.com>
2023-04-09 14:26:35 -0700

    Publication v2

--- app/src/main/java/com/example/tchafa/publication.kt
+++ app/src/main/java/com/example/tchafa/publication.kt
@@ -2,130 +2,117 @@ package com.example.tchafa
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.TextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SearchBar() {
+fun MaPage() {
     var searchText by remember { mutableStateOf("") }
-
     Surface(
-        color = Color.White,
-        modifier = Modifier.fillMaxWidth().padding(8.dp),
-        elevation = 8.dp,
-        shape = MaterialTheme.shapes.medium
+        color = Color(0xFF590D22),
+        modifier = Modifier.fillMaxSize()
     ) {
-        TextField(
-            value = searchText,
-            onValueChange = { searchText = it },
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = {
-                Text(
-                    text = "Rechercher...",
-                    color = Color.Gray
-                )
-            },
-            singleLine = true,
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = null
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.White,
-                cursorColor = Color.Black,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                text = "Titre de ma page",
+                style = MaterialTheme.typography.h4,
+                color = Color.White
             )
-        )
-    }
-}
-
-enum class ColorFilter(val color: Color) {
-    RED(Color.Red),
-    BLUE(Color.Blue),
-    GREEN(Color.Green)
-}
-
-@Composable
-fun FilterColors(onColorChanged: (Color) -> Unit) {
-    val selectedColor = remember { mutableStateOf(ColorFilter.RED) }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        ColorFilter.values().forEach { colorFilter ->
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                placeholder = { Text("Rechercher...") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
             Box(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(colorFilter.color)
-                    .clickable { selectedColor.value = colorFilter }
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(Color(0x80404040))
+                    .alpha(0.9f)
             ) {
-                if (selectedColor.value == colorFilter) {
-                    Text(
-                        text = "✓",
-                        color = Color.White,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    repeat(3) {
+                        ProfileBox(
+                            modifier = Modifier.padding(vertical = 16.dp)
+                        )
+                    }
                 }
             }
         }
     }
-
-    onColorChanged(selectedColor.value.color)
 }
 
 @Composable
-fun ProfileCard(
-    name: String,
-    firstName: String,
-    profileImageUrl: String,
-    onButton1Click: () -> Unit,
-    onButton2Click: () -> Unit,
-) {
+fun ProfileBox(modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier.fillMaxWidth().height(200.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.White)
+            .padding(16.dp)
+            .clickable { /* TODO: Action on click */ }
     ) {
-        Image(
-            painter = rememberImagePainter(profileImageUrl),
-            contentDescription = "Photo de profil",
-            modifier = Modifier.size(100.dp).clip(shape = CircleShape)
-                .align(alignment = Alignment.TopStart)
-        )
-        Column(modifier = Modifier.align(alignment = Alignment.TopCenter)) {
-            Text(text = name, fontWeight = FontWeight.Bold)
-            Text(text = firstName)
-        }
-        Row(
-            modifier = Modifier.align(alignment = Alignment.BottomCenter)
-                .padding(vertical = 16.dp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Button(onClick = onButton1Click) {
-                Text(text = "Bouton 1")
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = onButton2Click) {
-                Text(text = "Bouton 2")
+            Image(
+                painter = rememberImagePainter("https://images.unsplash.com/photo-1679499163638-a38b6c079595?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+                contentDescription = "Profile picture",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.White)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Nom Prénom",
+                style = MaterialTheme.typography.h5,
+                color = Color.White
+            )
+            Text(
+                text = "Description ou métier",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(onClick = { /* TODO: Action on button 1 click */ }) {
+                    Text(text = "Profil")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = { /* TODO: Action on button 2 click */ }) {
+                    Text(text = "Contact")
+                }
             }
         }
     }
 }
+
